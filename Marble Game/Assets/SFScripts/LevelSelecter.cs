8764296808beb8f8b5fce0b76a64be8a2f915672
dallasp83 <dallasp83@cableone.net>
2015-10-13 06:47:11 -0600

    Added Scene
    Level selecter scene has been added still working on funcrtionality

---
+++ Marble Game/Assets/SFScripts/LevelSelecter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelSelecter : MonoBehaviour 
+{
+	public Button level;
+
+	void Start()
+	{
+		level = level.GetComponent<Button> ();
+	}
+
+	public void StartLevel()
+	{
+		Application.LoadLevel (2);
+	}
+}
